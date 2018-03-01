4648966bfdaf1f9738a024c04e02ac5a54d52e4e
Gyanesh Malhotra <gyaneshmalhotra@gmail.com>
2018-03-01 23:26:06 +0530

    Fetch test_fast.py and then try

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -7,6 +7,8 @@ unset SSL_CERT_FILE
 
 python -m pip install -U pip nose
 pip install -U gensim
+wget https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py
+pwd
 python test_fast.py
 python -m unittest -v gensim.test.test_doc2vec.TestDoc2VecModel 
 nosetests --exe -v gensim
