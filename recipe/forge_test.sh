a86f2169e83ea2b041588207982f1959970557db
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-03-01 20:47:14 +0530

    Add tests in forge_test.sh

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -7,4 +7,6 @@ unset SSL_CERT_FILE
 
 python -m pip install -U pip nose
 pip install -U gensim
+python -m unittest -v test_fast.TestFastVersion
+python -m unittest -v gensim.test.test_doc2vec.TestDoc2VecModel 
 nosetests --exe -v gensim
