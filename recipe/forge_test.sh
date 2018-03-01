17f2041368f00455324101ea70b8f62f0be84ff5
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-03-01 22:00:18 +0530

    fix test_fast check

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -7,6 +7,6 @@ unset SSL_CERT_FILE
 
 python -m pip install -U pip nose
 pip install -U gensim
-python -m unittest -v test_fast.TestFastVersion
+python test_fast.py
 python -m unittest -v gensim.test.test_doc2vec.TestDoc2VecModel 
 nosetests --exe -v gensim
