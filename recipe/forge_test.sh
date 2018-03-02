a0d1e013070ede1f483175b4729c5c30faa449bb
Gyanesh Malhotra <gyaneshmalhotra@gmail.com>
2018-03-02 08:47:02 +0530

    Retry with curl

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -7,7 +7,7 @@ unset SSL_CERT_FILE
 
 python -m pip install -U pip nose
 pip install -U gensim
-wget https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py
+curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
 pwd
 python test_fast.py
 python -m unittest -v gensim.test.test_doc2vec.TestDoc2VecModel 
