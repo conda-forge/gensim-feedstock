ace8fd1761651c8eebb98dd047b557ee0b7be1c4
Gyanesh Malhotra <gyaneshmalhotra@gmail.com>
2018-03-03 11:53:01 +0000

    Fetch and debug doc2vec test

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -8,6 +8,7 @@ unset SSL_CERT_FILE
 python -m pip install -U pip nose
 pip install -U gensim
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
+curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_doc2vec.py>test_doc2vec.py
 python test_fast.py
-python gensim/test/test_doc2vec.py 
+python test_doc2vec.py 
 nosetests --exe -v gensim
