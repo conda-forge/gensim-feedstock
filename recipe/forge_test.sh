21bb7c0d4981642d61aeef761163c1d556a03a76
Gyanesh Malhotra <gyaneshmalhotra@gmail.com>
2018-03-05 22:14:18 +0530

    Debug tests for word2vec and fasttext

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -8,7 +8,11 @@ unset SSL_CERT_FILE
 python -m pip install -U pip nose
 pip install -U gensim
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
-curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_doc2vec.py>test_doc2vec.py
+# curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_doc2vec.py>test_doc2vec.py
+curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_word2vec.py>test_word2vec.py
+curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_fasttext.py>test_fasttext.py
 python test_fast.py
-python test_doc2vec.py 
+# python test_doc2vec.py 
+python test_word2vec.py
+python test_fasttext.py
 nosetests --exe -v gensim
