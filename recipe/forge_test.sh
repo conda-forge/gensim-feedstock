2efbe6867dfb96c381ab73590b8b1d5ac03751f4
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-03-12 17:52:50 +0530

    Test for complete w2v

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -8,11 +8,9 @@ unset SSL_CERT_FILE
 python -m pip install -U pip nose
 pip install -U gensim
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
-curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_w2v.py>test_w2v.py
 
 python test_fast.py
 # python -m gensim.test.test_doc2vec 
 # python -m gensim.test.test_fasttext
-#python -m unittest gensim.test.test_word2vec
-python test_w2v.py
+python -m unittest gensim.test.test_word2vec
 #nosetests --exe -v gensim
