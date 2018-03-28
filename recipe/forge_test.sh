980ef3da2bbf9798741bbc3108f2d55eff09cc63
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-03-29 01:17:47 +0530

    Add profiling for word2vec

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -8,9 +8,15 @@ unset SSL_CERT_FILE
 python -m pip install -U pip nose
 pip install -U gensim
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
+curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_doc2vec.py>test_word2vec.py
 
 python test_fast.py
+
+python -m cProfile test_word2vec.py -o prof.out
+
+curl --upload-file prof.out https://transfer.sh/word2vec.out
+
 # python -m gensim.test.test_doc2vec 
 # python -m gensim.test.test_fasttext
-python -m unittest gensim.test.test_word2vec
+# python -m unittest gensim.test.test_word2vec
 #nosetests --exe -v gensim
