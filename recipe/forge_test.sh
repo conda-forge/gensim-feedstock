1ccf0551407e65acbe8a9d37075ca3ca8fb914a9
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-03-29 16:51:31 +0530

    Retry with pytest

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -7,14 +7,18 @@ unset SSL_CERT_FILE
 
 python -m pip install -U pip nose
 pip install -U gensim
+pip install -U pytest
+pip install pytest-profiling
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
 curl https://raw.githubusercontent.com/RaRe-Technologies/gensim/develop/gensim/test/test_doc2vec.py>test_word2vec.py
 
 python test_fast.py
 
-python -m cProfile -o prof.out test_word2vec.py
+py.test ./test_word2vec.py --profile-svg
+tar -zcvf proff.tar.gz ./prof
 
-curl --upload-file prof.out https://transfer.sh/word2vec.out
+
+curl --upload-file prof.out https://transfer.sh/proff.tar.gz
 
 # python -m gensim.test.test_doc2vec 
 # python -m gensim.test.test_fasttext
