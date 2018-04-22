b7de83622759e6997f0f037c7a41c11268f92329
gyanesh-m <gyaneshmalhotra@gmail.com>
2018-04-22 10:58:29 +0530

    Add path information

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -5,6 +5,23 @@ set -e -x
 unset REQUESTS_CA_BUNDLE
 unset SSL_CERT_FILE
 
+export OPENBLAS_NUM_THREADS=1
+
+
+cat > site.cfg << EOF
+
+[DEFAULT]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+[atlas]
+atlas_libs = openblas
+libraries = openblas
+[openblas]
+libraries = openblas
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+EOF
+
 python -m pip install -U pip nose
 pip install -U gensim
 curl https://raw.githubusercontent.com/gyanesh-m/gensim-feedstock/osx-build-test/recipe/test_fast.py>test_fast.py
--- recipe/profiler.py
+++ recipe/profiler.py
@@ -1,8 +1,4 @@
 import cProfile
 from gensim.test.test_word2vec import *
 import sys
-try:
-	os.environ["OPENBLAS_NUM_THREADS"] = "1"
-except Exception as e:
-	print(e)
 cProfile.run("unittest.main()","combined-"+str(sys.version_info[0])+"."+str(sys.version_info[1]))
